500a72d8086e09358413e7216c639e1d217ee896
Sourabh <hegdesourabh02@gmail.com>
2023-08-23 23:22:35 +0530

    Image search ann css changes

--- server.js
+++ server.js
@@ -10,11 +10,34 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+const wordToEmoji = {
+  "woah": "😲",
+  "hey": "👋",
+  "lol": "😂",
+  "like": "🤍",
+  "congratulations": "🎉",
+  "react":"🔯"  
+  // Replace "woah" with the corresponding emoji
+  // Add more word-emoji mappings as needed
+};
+
 io.on('connection', (socket) => {
   console.log('A user connected');
   
   socket.on('message', (msg) => {
-    io.emit('message', msg); // Broadcast the message to all clients
+    const originalMsg = msg; // Store the original message
+    
+    // Convert the message to lowercase for case-insensitive comparison
+    msg = msg.toLowerCase();
+    
+    // Check if the message contains the word "woah" and replace it with the emoji
+    for (const word in wordToEmoji) {
+      if (msg.includes(word)) {
+        msg = msg.replace(word, wordToEmoji[word]);
+      }
+    }
+    
+    io.emit('message', msg); // Broadcast the modified message (with emoji) to all clients
   });
 
   socket.on('disconnect', () => {
